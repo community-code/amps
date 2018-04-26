baf9c4c65cfc302b4516e83848cfea32f859c553
community-code <coco.communitycode@gmail.com>
2018-04-25 18:11:46 -0700

    Update version number Fully remove ActivityTest

--- app/src/androidTest/java/com/communitycode/amps/main/ActivityTest.java
+++ app/src/androidTest/java/com/communitycode/amps/main/ActivityTest.java
@@ -1,34 +1,34 @@
 package com.communitycode.amps.main;
 
-
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
-
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
-
 //    Disable test as intermediate progress bar prevents espresso's getActivity from working.
-
+//
+//import android.support.test.espresso.ViewInteraction;
+//import android.support.test.rule.ActivityTestRule;
+//import android.support.test.runner.AndroidJUnit4;
+//import android.test.suitebuilder.annotation.LargeTest;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.view.ViewParent;
+//
+//import org.hamcrest.Description;
+//import org.hamcrest.Matcher;
+//import org.hamcrest.TypeSafeMatcher;
+//import org.junit.Rule;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//
+//import static android.support.test.InstrumentationRegistry.getInstrumentation;
+//import static android.support.test.espresso.Espresso.onView;
+//import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+//import static android.support.test.espresso.action.ViewActions.click;
+//import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+//import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+//import static android.support.test.espresso.matcher.ViewMatchers.withId;
+//import static android.support.test.espresso.matcher.ViewMatchers.withText;
+//import static org.hamcrest.Matchers.allOf;
+//import static org.hamcrest.Matchers.is;
+//
+//
 //@LargeTest
 //@RunWith(AndroidJUnit4.class)
 //public class ActivityTest {
